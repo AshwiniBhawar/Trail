46f1b612228583db665db7571bc3074d01146aa0
Ashwini <bhawar.ashwini2892@gmail.com>
2025-02-27 07:23:46 +0530

    clone

--- src/Test1/Test2.java
+++ src/Test1/Test2.java
@@ -1,3 +1,10 @@
-class test{
-System.out.println("I am in test2");
+package Test1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("I am happy");
+
+	}
+
 }
